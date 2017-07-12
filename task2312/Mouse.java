cddf707ab15b40a4f5399706718588dec794cc4b
Kharibin <ai.kharybin@gmail.com>
2017-07-12 16:28:12 +0300

    Start point
    Here we got some empty classes and Keyboard observer

---
+++ task2312/Mouse.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task23.task2312;
+
+/**
+ * Created by Java on 12.07.2017.
+ */
+public class Mouse {
+    int x, y;
+}
---
+++ task2312/Room.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task23.task2312;
+
+/**
+ * Created by Java on 12.07.2017.
+ */
+public class Room {
+    public static void main(String args[]){
+        
+    }
+
+}
+
